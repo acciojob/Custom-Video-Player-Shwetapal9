3776265ef7b49b9a11c0072dcd87f629b8f1b1b9
Shweta Pal <shwetapal975@gmail.com>
2023-06-16 00:42:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 /* Edit this file */
 const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
-const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const toggle = document.querySelector('.toggle');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const ranges = document.querySelectorAll('.player__slider');
+
+function toggleVideoStatus(){
+	if(video.paused){
+		video.play();
+	}
+	else{
+		video.pause(); 
+	}
+}
+function updatePlayIcon(){
+	if(video.paused){
+		toggle.innerHTML= '<button class="player__button toggle" title="Toggle Play">❚❚</button>'
+	}
+	else{
+		toggle.innerHTML= '<button class="player__button toggle" title="Toggle Play"> ► </button>'
+	}
+}
+function updateProgress(){
+	
+}
+ 
+video.addEventListener('click', toggleVideoStatus);
+toggle.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon); 
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate',updateProgress);
